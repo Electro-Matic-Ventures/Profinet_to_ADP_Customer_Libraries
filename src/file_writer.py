722d7d22a419d808165e2fe98a422ab35743edb5
jrmachado <jrmachado@electro-matic.com>
2023-03-11 10:58:32 -0500

    switching from mackbook to work laptop

--- src/file_writer.py
+++ src/file_writer.py
@@ -6,13 +6,14 @@
 from typing import Tuple
 import xlsxwriter
 from constants_table_row import ConstantsTableRow
+from tag_table_row import TagTableRow
 
 
 class FileWriter:
     """
         writes output excel file
     """
-    @staticmethod
+    
     def write(file_name:str, data:Tuple[ConstantsTableRow, ...])-> None:
         """
             writes to file
@@ -21,21 +22,43 @@ class FileWriter:
             @return None
         """
         work_book = xlsxwriter.Workbook(file_name)
-        work_sheet = work_book.add_worksheet(name='Constants')
+        constants_sheet = work_book.add_worksheet(name='Constants')
+        tags_sheet = work_book.add_worksheet(name='PLC Tags')
         row_ = 0
         col_ = 0
-        work_sheet.write(row_, col_, 'Name')
-        work_sheet.write(row_, col_+1, 'Path')
-        work_sheet.write(row_, col_+2, 'Data Type')
-        work_sheet.write(row_, col_+3, 'Value')
-        work_sheet.write(row_, col_+4, 'Comment')
-        row_ += 1
         for datum in data:
-            work_sheet.write(row_, col_, datum.name)
-            work_sheet.write(row_, col_+1, datum.path)
-            work_sheet.write(row_, col_+2, datum.data_type)
-            work_sheet.write(row_, col_+3, datum.value)
-            work_sheet.write(row_, col_+4, datum.comment)
             row_ += 1
+            if type(datum) == "ConstantsTableRow":
+                FileWriter.__constants(datum)
+                continue
+            if type(datum) == "TagTableRow":
+                FileWriter.__tags(datum)
+                continue
+        row_ += 1
         work_book.close()
         return
+        def __make_constants_sheet()->None:
+            work_sheet.write(row_, col_, 'Name')
+            work_sheet.write(row_, col_+1, 'Path')
+            work_sheet.write(row_, col_+2, 'Data Type')
+            work_sheet.write(row_, col_+3, 'Value')
+            work_sheet.write(row_, col_+4, 'Comment')
+            return
+        def __constants(row_:int, datum:ConstantsTableRow)-> None:
+            work_sheet.write(row_, 0, datum.name)
+            work_sheet.write(row_, 1, datum.path)
+            work_sheet.write(row_, 2, datum.data_type)
+            work_sheet.write(row_, 3, datum.value)
+            work_sheet.write(row_, 4, datum.comment)
+            return
+        def __tags(row_:int, datum:TagTableRow)-> None:
+            if datum.path == "Line Formatting":
+                FileWriter.__line_formatting()
+                return
+            FileWriter.__format_segment()
+            return
+        def __line_formatting()-> None:
+            
+            return
+        def __format_segment()-> None:
+            return
